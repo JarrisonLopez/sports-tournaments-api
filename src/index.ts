5748b9c1869361d7236231f5b82d9fd0ae6e7c03
JarrisonL <jarrilopezroldan@gmail.com>
2026-08-26 14:15:32 -0500

    ✨ feat: configurar servidor Fastify sencillo

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log("API de Torneos Deportivos");
+import Fastify from "fastify";
+
+const app = Fastify({
+  logger: true,
+});
+
+app.get("/", async () => {
+  return {
+    message: "API de Torneos Deportivos funcionando",
+  };
+});
+
+const start = async () => {
+  try {
+    await app.listen({
+      port: 3000,
+      host: "0.0.0.0",
+    });
+  } catch (error) {
+    app.log.error(error);
+    process.exit(1);
+  }
+};
+
+start();
