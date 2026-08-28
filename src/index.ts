9e5421a3812b840f7cc5c298a2c81f69d361a6f9
JarrisonL <jarrilopezroldan@gmail.com>
2026-08-28 01:10:13 -0500

    🔧 chore: configurar puerto para despliegue en nube

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ const start = async () => {
     app.log.info("Base de datos conectada");
 
     await app.listen({
-      port: 3000,
+      port: Number(process.env.PORT) || 3000,
       host: "0.0.0.0",
     });
   } catch (error) {
